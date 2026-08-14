8fe595ba1cba7b0b95323bc94f7b94b25c122a2a
cna0 <chanutha.adikary@outlook.com>
2026-08-14 20:14:26 +0100

    Testing struct by itself

--- main.cpp
+++ main.cpp
@@ -5,3 +5,15 @@ struct Block{
     bool is_free; //checks whether block is currently available
     Block* next; //pointer to another block
 };
+
+
+int main(){
+    Block block;
+    block.size = 100;
+    block.is_free = true;
+    block.next = nullptr;
+
+    std::cout << "Size: " << block.size << '\n';
+    std::cout << "Free: " << block.is_free << '\n';
+    std::cout << "Next: " << block.next << '\n';
+}
